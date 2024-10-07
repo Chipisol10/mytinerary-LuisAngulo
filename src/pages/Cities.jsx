eb8c45c8d49d0c686c57ab4d58f43ac682e6682d
Luis G <luisdavidnazareth@gmail.com>
2024-10-07 17:08:07 -0300

    Agrego el Archivo NavBar y  avanzo con Home y Cities

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,2 +1,14 @@
+import React from 'react';
 
+const Cities = () => {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-3xl text-white font-bold">Cities</h1>
+      <p className='text-white'>Sorry, this page is under construction </p>
+      
+    </div>
+  );
+};
+
+export default Cities;
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = () => {
+  return (
+    <div 
+      className="p-8 h-screen bg-cover bg-center flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold text-white drop-shadow-lg mb-4">Cities</h1>
+      <p className="text-xl text-white drop-shadow-md">This is where the city search will go.</p>
+    </div>
+  );
+};
+
+export default Home;
